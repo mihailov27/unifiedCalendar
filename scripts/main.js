63c8d4c736800a1556ea032fbffeb0948b1e5cc5
Mihail Mihaylov <msmihaylov89@gmail.com>
2015-05-03 15:33:34 +0300

    added swipe left-right handler for main container

--- scripts/main.js
+++ scripts/main.js
@@ -19,8 +19,9 @@ var MenuController = function(selectedView) {
 
 MenuController.prototype.bindEvents = function() {
 	console.log('Menu controller: binding events.');
-	$( "#daily, #weekly, #monthly" ).on("tap", this.tapHandler);
-	$("#calendar").fullCalendar({
+	$( "#daily, #weekly, #monthly" ).on('tap', this.tapHandler);
+	var calendarContainer = $('#calendar');
+	calendarContainer.fullCalendar({
 		theme: false,
 		header: {
 			left: '',
@@ -29,6 +30,8 @@ MenuController.prototype.bindEvents = function() {
 		},
 		defaultView: 'agendaDay'		
 	});
+	calendarContainer.on('swiperight', this.swipeRightHandler);
+	calendarContainer.on('swipeleft', this.swipeLeftHandler);
 	//$( "#daily" ).trigger( "tap" ); --> trigger tap event
 };
 
@@ -36,18 +39,25 @@ MenuController.prototype.tapHandler = function (event) {
 	console.log("Tap event received: " + event.target.id);
 	// update selected view
 	this.selectedView = event.target.id;
-	var calendar = $('#calendar');
-	if(this.selectedView === 'daily') {
-		$(document).ready(function(){
-			calendar.fullCalendar('changeView', 'agendaDay');			
-		});
-	} else if (this.selectedView === 'weekly') {
-		$(document).ready(function(){
-			calendar.fullCalendar('changeView', 'agendaWeek');		
-		});
-	} else if (this.selectedView === 'monthly') {
-		$(document).ready(function(){
-			calendar.fullCalendar('changeView', 'month');			
-		});
-	}
+	var calendarContainer = $('#calendar');
+	var that = this;
+	$(document).ready(function(){
+		if(that.selectedView === getDailyView()) {
+			calendarContainer.fullCalendar('changeView', 'agendaDay');
+		} else if (that.selectedView === getWeeklyView()) {
+			calendarContainer.fullCalendar('changeView', 'agendaWeek');
+		} else if (that.selectedView === getMonthlyView()) {
+			calendarContainer.fullCalendar('changeView', 'month');
+		}
+	});
+};
+
+MenuController.prototype.swipeLeftHandler = function(event) {
+	var calendarContainer = $('#calendar');
+	calendarContainer.fullCalendar('prev');
+};
+
+MenuController.prototype.swipeRightHandler = function(event) {
+	var calendarContainer = $('#calendar');
+	calendarContainer.fullCalendar('next');
 };
