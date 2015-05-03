6dc57f44c843b4ad0b918de8a99b52829fd05914
Mihail Mihaylov <msmihaylov89@gmail.com>
2015-05-03 15:06:23 +0300

    add full calendar library

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,53 @@
 $( document ).ready(function() {
     console.log( "ready!" );
-	//alert('ready');
-	var menuController = new MenuController();
-	menuController.initTabBar();
+	var menuController = new MenuController(getDailyView());
+	menuController.bindEvents();
 });
 
-var MenuController = function() {
-	this.initTabBar = function(){
-		alert('hello world');	
-	}
+// Constants for screen names
+
+function getDailyView() { return 'daily'; }
+function getWeeklyView() { return 'weekly'; }
+function getMonthlyView() { return 'monthly'; }
+
+// Menu controller
+
+var MenuController = function(selectedView) {
+	console.log('Menu controller initiated.');
+	this.selectedView = selectedView;
 }
+
+MenuController.prototype.bindEvents = function() {
+	console.log('Menu controller: binding events.');
+	$( "#daily, #weekly, #monthly" ).on("tap", this.tapHandler);
+	$("#calendar").fullCalendar({
+		theme: false,
+		header: {
+			left: '',
+			center: 'title',
+			right: ''
+		},
+		defaultView: 'agendaDay'		
+	});
+	//$( "#daily" ).trigger( "tap" ); --> trigger tap event
+};
+
+MenuController.prototype.tapHandler = function (event) {
+	console.log("Tap event received: " + event.target.id);
+	// update selected view
+	this.selectedView = event.target.id;
+	var calendar = $('#calendar');
+	if(this.selectedView === 'daily') {
+		$(document).ready(function(){
+			calendar.fullCalendar('changeView', 'agendaDay');			
+		});
+	} else if (this.selectedView === 'weekly') {
+		$(document).ready(function(){
+			calendar.fullCalendar('changeView', 'agendaWeek');		
+		});
+	} else if (this.selectedView === 'monthly') {
+		$(document).ready(function(){
+			calendar.fullCalendar('changeView', 'month');			
+		});
+	}
+};
